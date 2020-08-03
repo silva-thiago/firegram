f9ccd843b5c01f9ddad52fe9b99e4a41de7ad3e9
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-03 18:58:42 -0300

    Arquivos renomeados

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import PageTitle from '../comps/PageTitle'
+import Layout from '../comps/Layout'
+
+const Index = () => {
+  return (
+    <React.Fragment>
+      <PageTitle title='' />
+      <Layout />
+    </React.Fragment>
+  )
+}
+
+export default Index
