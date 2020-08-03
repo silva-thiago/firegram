9b1491488af19f4d3768decd554fdf62f7a9b57a
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-03 13:41:20 -0300

    Melhorias na animação

--- src/comps/ImageGrid.jsx
+++ src/comps/ImageGrid.jsx
@@ -11,8 +11,8 @@ const ImageGrid = ({ setSelectedImg }) => {
     <React.Fragment>
       <div className='img-grid'>
         {docs && docs.map(doc => (
-          <motion.div className='img-wrap' key={doc.id} onClick={() => setSelectedImg(doc.url)} layout whileHover={{ opacity: .75 }}>
-            <motion.img src={doc.url} alt='Uploaded file' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1 }} />
+          <motion.div className='img-wrap' key={doc.id} onClick={() => setSelectedImg(doc.url)} layout whileHover={{ opacity: .75, scale: 1.025, shadow: 1 }} whileTap={{ scale: 0.9, borderRadius: "25%" }}>
+            <motion.img src={doc.url} alt='Uploaded file' initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ type: "spring", velocity: 2, stiffness: 260, damping: 20, delay: .5 }} />
           </motion.div>
         ))}
       </div>
