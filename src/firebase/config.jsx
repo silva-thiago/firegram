a505f3e84e52876712d224526d8d79492dfdee01
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-03 18:59:20 -0300

    Correção de erro na inicialização do Firebase

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -14,7 +14,9 @@ const firebaseConfig = {
   measurementId: "G-P9XXTT8FC2"
 }
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
 // firebase.analytics()
 
 const projectStorage = firebase.storage()
